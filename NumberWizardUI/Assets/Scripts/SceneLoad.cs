c71ea05b911bd8a9de8f8f8d621f247c3d3d6576
Simon <spdrake2@sheffield.ac.uk>
2019-08-19 14:44:52 +0100

    Committing NumberWizardUI in VSCode

---
+++ NumberWizardUI/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class SceneLoad : MonoBehaviour
+{
+    public void loadNextScene(){
+
+        // Get scene index
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex + 1 );
+
+    }
+}
