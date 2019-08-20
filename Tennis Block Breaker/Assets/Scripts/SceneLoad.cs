2a797add1be1e9c1296461f406c1496bf2beaffc
Simon <s.drake03294@gmail.com>
2019-08-20 13:53:46 +0100

    Starting Block Breaker

---
+++ Tennis Block Breaker/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoad : MonoBehaviour
+{
+    public void loadNextScene(){
+
+        // Get scene index
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex + 1 );
+    }
+
+    public void loadStartScene(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void quitGame(){
+        Application.Quit();
+    }
+}
