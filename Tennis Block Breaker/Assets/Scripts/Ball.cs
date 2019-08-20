a1eb1862b9292617a3c533e368065b12dfa299f6
Simon <s.drake03294@gmail.com>
2019-08-20 17:40:56 +0100

    Lose Collision is working

---
+++ Tennis Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour{
+    [SerializeField] Paddle paddle;
+
+    Vector2 paddleToBall;
+
+    bool hadStarted = false;
+
+    // Start is called before the first frame update
+    void Start(){
+        paddleToBall = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+
+    void Update(){
+        if(!hadStarted){
+            LockToPaddle();
+            LaunchOnClick();
+        }
+    }
+
+    private void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+            hadStarted = true;
+        }
+    }
+
+    void LockToPaddle(){
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBall;
+    }
+}
+        
--- Tennis Block Breaker/Assets/Scripts/Paddle.cs
+++ Tennis Block Breaker/Assets/Scripts/Paddle.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour{
 
-    [SerializeField] float screenWidthUnits = 16;
+    [SerializeField] float screenWidthUnits = 16f;
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        minX = 1f;
+        maxX = 15f;
     }
 
     // Update is called once per frame
     void Update(){
-        Debug.Log(Input.mousePosition.x / Screen.width * screenWidthUnits);
+        float mouseXPos = Input.mousePosition.x / Screen.width * screenWidthUnits;
+        Vector2 paddlePos  = new Vector2(Mathf.Clamp(mouseXPos, minX, maxX), 0);
+        transform.position = paddlePos;
     }
 }
