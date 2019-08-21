ee6635cdc48f59dc31aea5483d2a9f9efa952945
Simon <s.drake03294@gmail.com>
2019-08-21 13:50:43 +0100

    Inserting audio

--- Tennis Block Breaker/Assets/Scripts/Ball.cs
+++ Tennis Block Breaker/Assets/Scripts/Ball.cs
@@ -7,20 +7,25 @@ public class Ball : MonoBehaviour{
     [SerializeField] Paddle paddle;
     [SerializeField] float releaseVectorX = 2f;
     [SerializeField] float releaseVectorY = 14f;
+    [SerializeField] AudioClip[] ballSounds;
 
     Vector2 paddleToBall;
 
-    bool hadStarted = false;
+    //Cached component references
+    AudioSource myAudioSource;
+
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start(){
+        myAudioSource = GetComponent<AudioSource>();
         paddleToBall = transform.position - paddle.transform.position;
     }
 
     // Update is called once per frame
 
     void Update(){
-        if(!hadStarted){
+        if(!hasStarted){
             LockToPaddle();
             LaunchOnClick();
         }
@@ -29,13 +34,20 @@ public class Ball : MonoBehaviour{
     private void LaunchOnClick(){
         if (Input.GetMouseButtonDown(0)){
             GetComponent<Rigidbody2D>().velocity = new Vector2(releaseVectorX, releaseVectorY);
-            hadStarted = true;
+            hasStarted = true;
         }
     }
 
     void LockToPaddle(){
         Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePos + paddleToBall;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (hasStarted){
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
     }
 }
         
