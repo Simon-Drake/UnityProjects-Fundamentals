7c9cbf365f889a1f26cb8147774b37179d114681
Simon <s.drake03294@gmail.com>
2019-08-22 14:51:31 +0100

    Fully functioning game (still needs tuning)

--- Tennis Block Breaker/Assets/Scripts/Block.cs
+++ Tennis Block Breaker/Assets/Scripts/Block.cs
@@ -6,8 +6,16 @@ public class Block : MonoBehaviour{
 
     [SerializeField] AudioClip breakSound;
 
+    Level level;
+
+    public void Start(){
+        level = FindObjectOfType<Level>();
+        level.countBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision){
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
---
+++ Tennis Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int unbrokenBlocks;
+
+    SceneLoad sceneLoader;
+
+    public void Start(){
+        sceneLoader = FindObjectOfType<SceneLoad>();
+    }
+    public void countBreakableBlocks(){
+        unbrokenBlocks ++;
+    }
+
+    public void BlockDestroyed(){
+        unbrokenBlocks -- ;
+        if (unbrokenBlocks <= 0){
+            sceneLoader.loadNextScene();
+        }
+    }
+}
