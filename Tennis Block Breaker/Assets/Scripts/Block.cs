5ff4891ff62528822b6f753ab8ebf1377220a3ff
Simon <s.drake03294@gmail.com>
2019-09-06 12:10:15 +0100

    unbreakable blocks and heavy blocks

--- Tennis Block Breaker/Assets/Scripts/Block.cs
+++ Tennis Block Breaker/Assets/Scripts/Block.cs
@@ -1,38 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour{
 
-    // Reference variables
+    // Configuration parameters
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
+
+    // Cached references
     Level level;
+
+    // State variables 
+    [SerializeField] int timesHit; //(serialised for debug)
 
     public void Start()
     {
-        // Called on every creation of a Block instance
-        // Sums unbroken blocks
-        level = FindObjectOfType<Level>();
-        level.countBreakableBlocks();
+        if(tag == "Breakable")
+        {
+            // Called on every creation of a Block instance
+            // Sums unbroken blocks
+            level = FindObjectOfType<Level>();
+            level.countBreakableBlocks();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(tag == "Breakable")
+        {
+            timesHit++;
+            if(timesHit >= maxHits){
+                // Add point to player score. 
+                FindObjectOfType<GameState>().AddToScore();
+                
+                // Play sound
+                AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
 
-        // Add point to player score. 
-        FindObjectOfType<GameState>().AddToScore();
-        
-        // Play sound
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+                // Destroy
+                Destroy(gameObject);
+                level.BlockDestroyed();
 
-        // Destroy
-        Destroy(gameObject);
-        level.BlockDestroyed();
+                // Create particle effect
+                TriggerSparkles();
+            }
+            else
+            {
+                ShowNextHitSprite();
+            }
+        }
+    }
 
-        // Create particle effect
-        TriggerSparkles();
-
+    // Update sprite to display breakage. 
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     // Instantiates particle effect prefab
